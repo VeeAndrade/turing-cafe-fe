f70234ce0b3fe51e99a11866500dfb7c743e87dd
Veronica Andrade <veronica726@gmail.com>
2020-02-12 10:09:47 -0700

    Add class names to the html element tags

--- src/ReservationCard/ReservationCard.js
+++ src/ReservationCard/ReservationCard.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import './ReservationCard.css'
 
 const ReservationCard = ({ reservation }) => {
 return (
   <section key={reservation.id} className='reservation-card'>
-    <h3>{reservation.name} </h3>
-    <p>{reservation.date}</p>
-    <p>Number of guest: {reservation.number} </p>
+    <h3 className='reservation-detail'>{reservation.name} </h3>
+    <p className='reservation-detail'>{reservation.date}</p>
+    <p className='reservation-detail'>{reservation.time} pm</p>
+    <p className='reservation-detail'>Number of guest: {reservation.number} </p>
     <button className='cancel-reservation-button'>Cancel</button>
   </section>
-)
+  )
 }
 
 export default ReservationCard;
