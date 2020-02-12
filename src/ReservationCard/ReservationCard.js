4cbbbb161158a06ff7e3ec8be700f4f4d7007b10
Veronica Andrade <veronica726@gmail.com>
2020-02-12 09:26:50 -0700

    Create a base layout for the individual reservation cards

---
+++ src/ReservationCard/ReservationCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ReservationCard = () => {
+return (
+  <section className='reservation-card'>
+    <h3>name</h3>
+    <p>date</p>
+    <p>Number of guest: 5</p>
+    <button className='cancel-reservation-button'>Cancel</button>
+  </section>
+)
+}
+
+export default ReservationCard;
