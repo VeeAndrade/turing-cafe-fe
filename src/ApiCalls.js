446273db5cb7ac32d1ce855393385ef69dd264be
Veronica Andrade <veronica726@gmail.com>
2020-02-12 11:20:59 -0700

    Write out method to post reservation

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -7,3 +7,20 @@ export const getReservations = () => {
       return response.json()
     })
 }
+
+export const postReservation = (reservation) => {
+  const option = {
+    method: 'POST',
+    body: JSON.stringify({reservation}),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  return fetch('http://localhost:3001/api/v1/reservations', option)
+    .then(response => {
+      if(!response.ok) {
+        throw Error('Failure to make reservation')
+      }
+      return response.json()
+    })
+}
